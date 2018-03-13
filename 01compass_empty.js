2cb6e7d87fb1d2afa83e84562db7e64fcda3ed81
Maria Luisa <marialuisa@temps-MacBook-Pro.local>
2018-03-13 14:24:38 -0500

    added audio

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -11,7 +11,7 @@ oriented = function(e) {
 
         if (compassHeading > 315 || compassHeading < 45) {
          text_area.textContent = "some photographs are";
-         	audio.play();
+         if(canPlay) audio.play();
         }
 
         if (compassHeading >= 45 && compassHeading < 135) {
@@ -49,10 +49,19 @@ var text_area = document.getElementById("compass_content");
 
 	audio.onplaying = function() {
     	console.log("playing")
+    	isPlaying = true;
 };
 
 	audio.onended = function() {
     	alert("The audio has ended");
+    	isPlaying = false;
 	};
+
+	vid.oncanplay = function() {
+    canPlay = true;
+};
+
+	var isPlaying = false;
+	var canPlay = false;
 
 
